29588ac2d824367177996e4656cd72834e4e071a
BarbaNesquick <gorka@alexsancho.name>
2025-12-28 16:53:48 +0100

    He retocat 2 cosas

--- src/Sancio.java
+++ src/Sancio.java
@@ -4,4 +4,18 @@ public class Sancio {
     private String TipusSancio;
     private Jugador Sancionat;
     private Date HoraExpulsio;
+    public Sancio(String targeta,Jugador j,Date data){
+        this.TipusSancio=targeta;
+        this.Sancionat=j;
+        this.HoraExpulsio=data;
+    }
+    public String getTipusSancio() {
+        return TipusSancio;
+    }
+    public Jugador getSancionat() {
+        return Sancionat;
+    }
+    public Date getHoraExpulsio() {
+        return HoraExpulsio;
+    }
 }
