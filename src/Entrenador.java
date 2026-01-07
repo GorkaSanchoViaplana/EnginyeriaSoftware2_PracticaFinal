e69cb4695208f28b84d457e1824cfdb8f3e4bb85
BarbaNesquick <gorka@alexsancho.name>
2026-01-07 15:45:19 +0100

    Enviat missatge jugadors fet, arreglat constructor equip

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -2,17 +2,17 @@ import java.util.Observer;
 import java.util.ArrayList;
 
 public class Entrenador extends Participants implements ObserverArbitre,ObserverPartit,SubjectEntrenador {
-    private Equip meuEquip;
-    private Partit partitJugant;
+    //private Equip meuEquip;
+    //private Partit partitJugant;
     private ArrayList<ObserverEntrenador> llistaObservers; //Nomes seran els que estan al camp
 
-    public Entrenador(Equip meuEq,String nom,String cognom,String numfed,Partit partit) {
-        this.meuEquip = meuEq;
+    public Entrenador(String nom,String cognom,String numfed) {
+       // this.meuEquip = meuEq;
         llistaObservers = new ArrayList<ObserverEntrenador>();
         this.Nom = nom;
         this.Cognom = cognom;
         this.NumFed = numfed;
-        this.partitJugant =partit;
+        //this.partitJugant =partit;
     }
 
     public void donarOrdre(String missatge){
@@ -29,7 +29,7 @@ public class Entrenador extends Participants implements ObserverArbitre,Observer
     @Override
     public void notifica(String missatge) {
         for(ObserverEntrenador o : llistaObservers){
-            o.actualitzaEntrenador("Pinga");
+            o.actualitzaEntrenador(missatge);
         }
     }
 
--- src/Equip.java
+++ src/Equip.java
@@ -17,24 +17,28 @@ public class Equip {
             for(int i = 0; i < pista.length; i++){
                 if(pista[i] == null){
                     pista[i] = jug;
+                    EntrenadorEquip.donaAlta(jug);
+                    break;
                 }
             }
         } else if(estat.equals("b")){
             for(int i = 0; i < banquillo.length; i++){
                 if(banquillo[i] == null){
                     banquillo[i] = jug;
+                    break;
                 }
             }
         }
 
     }
-    Equip(String nom, String localitat){
+    Equip(String nom, String localitat,Entrenador entrenadorEquip){
         this.pista= new  Jugador[7];
         this.banquillo= new  Jugador[5];
         this.Nom = nom;
         this.Localitat = localitat;
+        this.EntrenadorEquip = entrenadorEquip; //No ens es important qui sigui el entrenador per tant fem un placeholder
     }
-    public void enviarMissatge(){
-        EntrenadorEquip.donarOrdre();
+    public void enviarMissatge(String miss){
+        EntrenadorEquip.donarOrdre(miss);
     }
 }
